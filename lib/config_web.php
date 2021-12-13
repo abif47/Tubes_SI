028841c41883428ce5f8ca0751166334469846b8
Moh. Abib Safaqdillah <mohammadabif46@gmail.com>
2021-12-13 14:26:40 +0800

    Initial Commit

---
+++ lib/config_web.php
@@ -0,0 +1,4 @@
+<?php
+$web_url = "http://localhost/Apotik-master/";
+$admin_url = "http://localhost/Apotik-master/adminpages/";
+?>
---
+++ lib/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database_name = "apoteksf1";
+
+$koneksi = mysqli_connect($host, $username, $password, $database_name) or die ("Koneksi Gagal");
+
+
+?>
